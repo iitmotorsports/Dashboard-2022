b24a07f4190afa8b3d0edee3aea8e2ad0491e3e3
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-08-08 21:18:04 -0500

    Custom Button Styles

---
+++ app/src/main/java/com/iit/dashboard2022/ui/SidePanel.java
@@ -0,0 +1,33 @@
+package com.iit.dashboard2022.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.iit.dashboard2022.R;
+
+public class SidePanel extends ConstraintLayout {
+    public SidePanel(Context context) {
+        super(context);
+        init();
+    }
+
+    public SidePanel(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SidePanel(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        View.inflate(getContext(), R.layout.side_panel_layout, this);
+    }
+}
---
+++ app/src/main/java/com/iit/dashboard2022/ui/button/SideButton.java
@@ -0,0 +1,24 @@
+package com.iit.dashboard2022.ui.button;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.iit.dashboard2022.R;
+
+public class SideButton extends com.google.android.material.button.MaterialButton {
+
+    public SideButton(@NonNull Context context) {
+        super(context, null, R.attr.sideButtonStyle);
+    }
+
+    public SideButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs, R.attr.sideButtonStyle);
+    }
+
+    public SideButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, R.attr.sideButtonStyle);
+    }
+}
