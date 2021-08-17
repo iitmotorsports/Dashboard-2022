232bea3b241fe0b6d4f76877030f433fec30a3a8
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-08-17 00:17:26 -0500

    shorten settle time

--- app/src/main/java/com/iit/dashboard2022/ui/widget/gauge/GaugeUpdater.java
+++ app/src/main/java/com/iit/dashboard2022/ui/widget/gauge/GaugeUpdater.java
@@ -14,7 +14,7 @@ public class GaugeUpdater {
     private static final HandlerThread gaugeThread = new HandlerThread("Gauge Thread");
     private static Handler gaugeHandler;
 
-    private static final long SETTLE_TIME_MS = 800;
+    private static final long SETTLE_TIME_MS = 500;
     private static long settleTime = 0;
     private static long lastTime = 0;
 
