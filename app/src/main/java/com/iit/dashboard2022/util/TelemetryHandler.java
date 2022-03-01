aa69364591b7fa35c642e6ef7653fed071b10956
Noah Husby <noahhusby@gmail.com>
2022-03-01 14:33:46 -0600

    Created base of TelemetryHandler

--- app/src/main/java/com/iit/dashboard2022/util/TelemetryHandler.java
+++ app/src/main/java/com/iit/dashboard2022/util/TelemetryHandler.java
@@ -1,5 +1,11 @@
 package com.iit.dashboard2022.util;
 
+import com.husbylabs.warptables.WTClient;
+import com.husbylabs.warptables.WarpTableInstance;
+import com.husbylabs.warptables.WarpTablesAPI;
+
+import java.net.InetSocketAddress;
+
 /**
  * A telemetry handler utilizing the WarpTables API
  *
@@ -12,7 +18,43 @@ public class TelemetryHandler {
 
     private static final TelemetryHandler instance = new TelemetryHandler();
 
+    private TelemetryHandler() {
+    }
 
+    private WarpTableInstance warpTableInstance = null;
+
+    /**
+     * Starts the WarpTables instance
+     *
+     * @param address Address of remote server
+     */
+    public void start(InetSocketAddress address) throws Exception {
+        if(warpTableInstance != null && ((WTClient) warpTableInstance).isConnected()) {
+            // TODO: Log
+            warpTableInstance.stop();
+        }
+        warpTableInstance = WarpTablesAPI.createClient(address);
+        ((WTClient) warpTableInstance).enableAutoConnect();
+        warpTableInstance.start();
+    }
+
+    /**
+     * Stops the WarpTables instance
+     */
+    public void stop() {
+        if(warpTableInstance != null) {
+            warpTableInstance.stop();
+        }
+    }
+
+    /**
+     * Checks if the WarpTables instance is connected
+     *
+     * @return True if connected, false if not
+     */
+    public boolean isConnected() {
+        return warpTableInstance != null && ((WTClient) warpTableInstance).isConnected();
+    }
 
     /**
      * Gets the instance of the TelemetryHandler
