f4c6b6fac9de6a316224f079140657296e356cd2
Noah Husby <noahhusby@gmail.com>
2022-03-29 08:45:21 -0500

    Changed API to new URL

--- app/src/main/java/com/iit/dashboard2022/util/Constants.java
+++ app/src/main/java/com/iit/dashboard2022/util/Constants.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
  */
 public class Constants {
     // Cabinet
-    public static final String CABINET_API = "https://logs.noahhusby.com/api/v1";
+    public static final String CABINET_API = "https://logs.iitmotorsports.org/api/v1";
     public static final String LINE_FEED = "\r\n";
 
     // JSON
